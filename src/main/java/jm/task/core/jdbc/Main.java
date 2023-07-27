245aa4daaf21bde6bbd6dde549eb023e9497dbef
alexeymoskalev <alexeymoskaleff@gmail.com>
2023-07-28 01:17:05 +0300

    echo "# PP_1_1_3-4_JDBC_Hibernate" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/alexeymoskalev/PP_1_1_3-4_JDBC_Hibernate.git git push -u origin main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,20 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+        userService.saveUser("Alexey", "Moskalev", (byte) 29);
+        userService.saveUser("Ivan","Ivanov", (byte) 18);
+        userService.saveUser("Petr", "Petrov", (byte) 33);
+        userService.saveUser("Viktor","Korneplod", (byte) 67);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
-}
+    }
+
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,39 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
-
+    UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+    public void createUsersTable() throws SQLException {
+        userDao.createUsersTable();
     }
 
-    public void dropUsersTable() {
-
+    public void dropUsersTable() throws SQLException {
+        userDao.dropUsersTable();
     }
 
-    public void saveUser(String name, String lastName, byte age) {
-
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
+        userDao.saveUser(name, lastName, age);
+        System.out.println("User с именем - " + name + " добавлен в базу данных");
     }
 
-    public void removeUserById(long id) {
-
+    public void removeUserById(long id) throws SQLException {
+        userDao.removeUserById(id);
     }
 
-    public List<User> getAllUsers() {
-        return null;
+    public List<User> getAllUsers() throws SQLException {
+        List<User> userList = userDao.getAllUsers();
+        for (User s : userList ) {
+            System.out.println(s);
+        }
+        return userList;
     }
 
-    public void cleanUsersTable() {
-
+    public void cleanUsersTable() throws SQLException {
+        userDao.cleanUsersTable();
     }
 }
