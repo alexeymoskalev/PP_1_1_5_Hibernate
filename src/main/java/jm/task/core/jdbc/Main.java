5f7f656fa0dea1646a30967ab50267f8bd3836a0
Alexey Moskalev <alexeymoskaleff@gmail.com>
2023-07-29 19:20:23 +0300

    Исправленный

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,10 +1,9 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.service.UserServiceImpl;
-import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         UserServiceImpl userService = new UserServiceImpl();
 
         userService.createUsersTable();
@@ -12,6 +11,7 @@ public class Main {
         userService.saveUser("Ivan","Ivanov", (byte) 18);
         userService.saveUser("Petr", "Petrov", (byte) 33);
         userService.saveUser("Viktor","Korneplod", (byte) 67);
+        userService.removeUserById(2);
         userService.getAllUsers();
         userService.cleanUsersTable();
         userService.dropUsersTable();
